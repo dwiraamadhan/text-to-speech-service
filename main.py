6bcc3eafdbd29179093f9e9a318065fd41040266
Dwi Ramadhan <dwiramadhan50@gmail.com>
2024-08-08 02:46:36 +0700

    refactor: remove .env from staging area

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from api.TTS import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["http://localhost:5173", "http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
